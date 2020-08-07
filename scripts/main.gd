0b75531b7a602826bbb4d368f66a6443fb446c9e
theowiik <42991003+theowiik@users.noreply.github.com>
2020-08-07 21:01:55 +0200

    Create basic logic for spawning the cards

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,4 +1,16 @@
 extends Node2D
 
+onready var card_container: ItemList = $CardContainer
+onready var color_card: PackedScene = preload("res://scenes/ColorCard.tscn")
+
 func _ready():
-  pass
+  var dic: Dictionary = {"blue": "#0000ff", "red": "#ff0000"}
+  add_cards(dic)
+
+func add_cards(colors: Dictionary) -> void:
+  for colorKey in colors.keys():
+    var hexCode: String = colors[colorKey]
+    
+    var card: ColorCard = color_card.instance()
+    card_container.add_child(card)
+    card.set_text(colorKey)
