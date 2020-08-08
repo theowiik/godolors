b7cc3a1bbd58f6e3641204c7939c43fbd2b5fe2f
theowiik <42991003+theowiik@users.noreply.github.com>
2020-08-08 12:20:46 +0200

    Set card color and set window size to only fit the cards

--- scripts/color_card.gd
+++ scripts/color_card.gd
@@ -8,6 +8,14 @@ func set_color(color: Color) -> void:
   if color == null: return
   rect.color = color
 
+func set_color_hex(hex: String) -> void:
+  set_color(Color(hex))
+
 func set_text(msg: String) -> void:
   if msg == null: return
   label.text = msg
+
+func set_card_size(width: int, height: int) -> void:
+  var size: Vector2 = Vector2(width, height)
+  rect_min_size = size
+  rect.rect_min_size = size
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,16 +1,35 @@
 extends Node2D
 
-onready var card_container: ItemList = $CardContainer
+onready var card_container: GridContainer = $CardContainer
 onready var color_card: PackedScene = preload("res://scenes/ColorCard.tscn")
+const COLUMNS: int = 4
+const CARD_WIDTH: int = 64
+const CARD_HEIGHT: int = 128
 
 func _ready():
-  var dic: Dictionary = {"blue": "#0000ff", "red": "#ff0000"}
+  var dic: Dictionary = get_color_hex_dic()
   add_cards(dic)
+  set_columns(COLUMNS)
+
+func _process(delta):
+  set_window_size() # should not be here :), but I am probably not gonna fix it
+
+func get_color_hex_dic() -> Dictionary:
+  return {"blue": "#0000ff", "red": "#ff0000"}
 
 func add_cards(colors: Dictionary) -> void:
-  for colorKey in colors.keys():
-    var hexCode: String = colors[colorKey]
+  for key in colors.keys():
+    var hexCode: String = colors[key]
     
     var card: ColorCard = color_card.instance()
     card_container.add_child(card)
-    card.set_text(colorKey)
+    card.set_text(key)
+    card.set_color_hex(hexCode)
+    card.set_card_size(CARD_WIDTH, CARD_HEIGHT)
+
+func set_columns(n: int) -> void:
+  if n <= 0: return
+  card_container.columns = n
+
+func set_window_size() -> void:
+  OS.window_size = card_container.rect_size
