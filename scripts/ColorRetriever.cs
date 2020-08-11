5c09921d62c7b7d16cb0b5ffa2df9c2c3721a727
theowiik <42991003+theowiik@users.noreply.github.com>
2020-08-11 22:43:15 +0200

    Retrieve files from colors.txt via c#

--- scripts/ColorRetriever.cs
+++ scripts/ColorRetriever.cs
@@ -1,18 +1,52 @@
 using Godot;
-using System;
-using System.Collections.Generic;
 using Godot.Collections;
 
-public class ColorRetriever : Node
+public sealed class ColorRetriever : Node
 {
-  public Array<Godot.Collections.Dictionary<string, Color>> GetSortedColors()
+  private const string FileName = "colors.txt";
+
+  public Array<Dictionary<string, Color>> GetSortedColors()
   {
-    return new Array<Godot.Collections.Dictionary<string, Color>>
+    var colors = ParseFile();
+    Sort(colors);
+    return colors;
+  }
+
+  private void Sort(Array<Dictionary<string, Color>> colors)
+  {
+  }
+
+  private Array<Dictionary<string, Color>> ParseFile()
+  {
+    var output = new Array<Dictionary<string, Color>>();
+
+    var file = new File();
+    file.Open(FileName, File.ModeFlags.Read);
+
+    while (!file.EofReached())
     {
-      new Godot.Collections.Dictionary<string, Color>
+      var line      = file.GetLine();
+      var colorName = line.Split(" ")[0];
+      var regex     = new RegEx();
+      regex.Compile("\\( .* \\)");
+      var result          = regex.Search(line);
+      var colorStr        = result.Strings[0] as string;
+      var cleanedColorStr = colorStr.Replace("(", "").Replace(")", "").Replace(" ", "");
+      var splitted        = cleanedColorStr.Split(",");
+
+      var dic = new Dictionary<string, Color>
       {
-        ["blue"] = new Color(0, 0, 1)
-      }
-    };
+        [colorName] = new Color(
+          float.Parse(splitted[0]),
+          float.Parse(splitted[1]),
+          float.Parse(splitted[2]),
+          float.Parse(splitted[3])
+        )
+      };
+
+      output.Add(dic);
+    }
+
+    return output;
   }
 }
